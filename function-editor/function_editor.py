77ccb99415110053fd346f548c5ed0f0f03dc0c8
chee_mac <kakao_ent@Kakao-entui-MacBookPro.local>
2022-11-02 13:55:05 +0900

    replace UI from svelte to streamlit

---
+++ function-editor/function_editor.py
@@ -0,0 +1,46 @@
+from kubernetes import client, config
+
+import yaml
+import streamlit as st
+
+st.header('Easy-Faas')
+
+api_client = None
+
+DEFINED_NAMESPACE = 'easy-faas'
+
+# Initialization(k8s connection)
+kubeconfig_login = st.empty()
+
+if "kubeconfig" not in st.session_state:
+    kubeconfig_input = kubeconfig_login.text_area(
+        label="kubeconfig :", placeholder="enter kubeconfig value",
+    )
+
+    if kubeconfig_input != '':
+        st.session_state["kubeconfig"] = yaml.safe_load(kubeconfig_input)
+
+        try:
+            config.load_kube_config_from_dict(st.session_state["kubeconfig"])
+            api_client = client.CoreV1Api()
+            custom_client = client.CustomObjectsApi()
+            kubeconfig_login.empty()
+            st.warning('k8s connection success!')
+
+            if api_client is not None:
+                # namespace / CRD / operator check
+                
+                ## check namespace
+                is_namespace_exist = False
+                namespace_list = api_client.list_namespace().items
+                for ns in namespace_list:
+                    if ns.metadata.name == DEFINED_NAMESPACE:
+                        is_namespace_exist = True
+                        break
+
+                ## check CRD
+                crd_list = custom_client.list_namespaced_custom_object
+
+        except:
+            st.error('k8s connection fail!')
+
