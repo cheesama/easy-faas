da47760917e6f397ef3e57273a73557743f80986
chee_mac <kakao_ent@Kakao-entui-MacBookPro.local>
2022-11-06 18:22:54 +0900

    implement custom resource creation flow

--- preparation/faas-operator.py
+++ preparation/faas-operator.py
@@ -3,4 +3,12 @@ import kopf
 import kubernetes
 import yaml
 
+CRD_NAME = "easy-faas.serverless"
+CRD_VERSION = "v1"
+KIND = "functions"
+
+@kopf.on.create(CRD_NAME, CRD_VERSION, KIND)
+def create(name, spec, namespace, logger, **kwargs):
+    logger.info(f"Function {name} is created")
+
 
