68bb3320ba8c1d9cfb1dca337db352f1618802bc
cheese.lee <cheese.lee@kakaoenterprise.com>
2022-12-03 23:38:06 +0900

    modify dependency as poetry

--- function-editor/function_editor.py
+++ function-editor/function_editor.py
@@ -3,44 +3,48 @@ from kubernetes import client, config
 import yaml
 import streamlit as st
 
-st.header('Easy-Faas')
+st.set_page_config(layout="wide")
+st.header("Easy-Faas")
 
 api_client = None
 
-DEFINED_NAMESPACE = 'easy-faas'
+DEFINED_NAMESPACE = "easy-faas"
 
-# Initialization(k8s connection)
-kubeconfig_login = st.empty()
+setting_tab, editor_tab, log_tab = st.tabs(["k8s setting", "Function Editor", "Logs"])
 
-if "kubeconfig" not in st.session_state:
-    kubeconfig_input = kubeconfig_login.text_area(
-        label="kubeconfig :", placeholder="enter kubeconfig value",
-    )
+with setting_tab:
+    # Initialization(k8s connection)
+    kubeconfig_login = st.empty()
 
-    if kubeconfig_input != '':
-        st.session_state["kubeconfig"] = yaml.safe_load(kubeconfig_input)
+    if "kubeconfig" not in st.session_state:
+        kubeconfig_input = kubeconfig_login.text_area(
+            label="kubeconfig :",
+            placeholder="enter kubeconfig value",
+        )
 
-        try:
-            config.load_kube_config_from_dict(st.session_state["kubeconfig"])
-            api_client = client.CoreV1Api()
-            custom_client = client.CustomObjectsApi()
-            kubeconfig_login.empty()
-            st.warning('k8s connection success!')
+        if kubeconfig_input != "":
+            st.session_state["kubeconfig"] = yaml.safe_load(kubeconfig_input)
 
-            if api_client is not None:
-                # namespace / CRD / operator check
-                
-                ## check namespace
-                is_namespace_exist = False
-                namespace_list = api_client.list_namespace().items
-                for ns in namespace_list:
-                    if ns.metadata.name == DEFINED_NAMESPACE:
-                        is_namespace_exist = True
-                        break
+            try:
+                config.load_kube_config_from_dict(st.session_state["kubeconfig"])
+                api_client = client.CoreV1Api()
+                custom_client = client.CustomObjectsApi()
+                kubeconfig_login.empty()
+                st.warning("k8s connection success!")
 
-                ## check CRD
-                crd_list = custom_client.list_namespaced_custom_object
+                if api_client is not None:
+                    # namespace / CRD / operator check
 
-        except:
-            st.error('k8s connection fail!')
+                    ## check namespace
+                    is_namespace_exist = False
+                    namespace_list = api_client.list_namespace().items
+                    for ns in namespace_list:
+                        if ns.metadata.name == DEFINED_NAMESPACE:
+                            is_namespace_exist = True
+                            break
 
+                    ## check CRD
+                    crd_list = custom_client.list_namespaced_custom_object
+
+            except:
+                st.error("k8s connection fail!")
